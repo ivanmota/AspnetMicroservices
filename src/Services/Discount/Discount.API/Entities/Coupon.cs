3ff32c8c0a65aa3d8ff5ecdf8876bc29623136d5
Ivan M <ivan.sm920@iguga.net>
2021-12-02 16:57:40 +0000

    Update discount.API

--- src/Services/Discount/Discount.API/Entities/Coupon.cs
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string ProductName { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public int Amount { get; set; }
     }
 }
