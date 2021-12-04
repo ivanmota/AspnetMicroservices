3af1e543f027a32be328b77efc889ec416265b8b
Ivan M <ivan.sm920@iguga.net>
2021-12-04 17:55:55 +0000

    Add Validation and Dependency ijection to the Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validtors;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validtors)
+        {
+            _validtors = validtors ?? throw new ArgumentNullException(nameof(validtors));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validtors.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validtors.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
