bf5e83a820dccaa41ad15decd90b09bdb7f06283
Ivan M <ivan.sm920@iguga.net>
2021-12-04 11:53:18 +0000

    Implementing DeleteOrder command on Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .NotEmpty().WithMessage("{Id} is required.")
+                .NotNull()
+                .GreaterThan(0).WithMessage("{Id} should be greater than zero.");
+        }
+    }
+}
